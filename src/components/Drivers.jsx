921479ed14cd373b30dae2d2289fb8521205abee
Srdjan-P <srdjan.potrebic@gmail.com>
2025-05-19 13:03:57 +0200

    add: drivers data

--- src/components/Drivers.jsx
+++ src/components/Drivers.jsx
@@ -1,7 +1,55 @@
-export default function AllDrivers () {
-    return(
-        <>
-        <p>All Drivers</p>
-        </>
+import axios from "axios";
+import { useEffect, useState } from "react"
+
+export default function AllDrivers() {
+    const [drivers, setDrivers] = useState([]);
+
+    useEffect(() => {
+        getDrivers();
+    }, [])
+
+    const getDrivers = async () => {
+        const url = "http://ergast.com/api/f1/2013/driverStandings.json";
+        const response = await axios.get(url);
+        const data = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+        setDrivers(data);
+        console.log(data);
+    }
+
+
+    return (
+        <div className="drivers">
+            <table>
+                <thead>
+                    <tr>
+                        <th colSpan={4}>
+                            <p>Drivers Championship Standings - 2013</p>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {drivers.map((driver) => {
+                        return (
+
+                            <tr>
+                                <td>
+                                    {driver.position}
+                                </td>
+                                <td>
+                                    {driver.Driver.givenName}
+                                </td>
+                                <td>
+                                    {driver.Constructors[0].name}
+                                </td>
+                                <td>
+                                    {driver.points}
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+
+        </div>
     )
 }
