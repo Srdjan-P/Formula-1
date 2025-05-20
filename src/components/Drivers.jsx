d01ad9cf3cc16654c1fc2839b2d68abac4fa9aa2
MarinaInCodeLand <jakov.marina@gmail.com>
2025-05-20 11:19:58 +0200

    DriverDetailsUpdate

--- src/components/Drivers.jsx
+++ src/components/Drivers.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router";
 
 
+
 export default function Drivers() {
     const [drivers, setDrivers] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
