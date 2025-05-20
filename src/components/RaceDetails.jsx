c11dbf59e07ce18d202e06461e3639fef5a4965f
DavorHajder <davorhajder@gmail.com>
2025-05-20 15:56:45 +0200

    Add Race Details

--- src/components/RaceDetails.jsx
+++ src/components/RaceDetails.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router";
+import Loader from "./Loader";
 
 
 
@@ -19,36 +20,55 @@ export default function RaceDetails() {
 
     const getQualifying = async () => {
         const urlqualifying = `http://ergast.com/api/f1/2013/${raceId}/qualifying.json`
-        const response = await axios.get(urlqualifying);
-        console.log(response);
-        // const data1 = response.data.MRData.StandingsTable.driverId
+        const qualifyingResponse = await axios.get(urlqualifying);
+        console.log(qualifyingResponse.data.MRData.RaceTable.Races[0].Results);
+        const data1 = qualifyingResponse.data.MRData;
+        setQualifying(data1);
 
-        // const urlresults = `http://ergast.com/api/f1/2013/${raceId}/results.json`
-        // const responseresult = await axios.get(urlresults);
-        // const data2 =
+        const urlresults = `http://ergast.com/api/f1/2013/${raceId}/results.json`
+        const resultResponse = await axios.get(urlresults)
+        console.log(resultResponse);
+        const data2 = [];
+        setRace(data2);
 
-        //     setQualifying(data1);
-        // setRace(data2);
-        // setIsLoading(false);
-
-
+        setIsLoading(false);
     };
-
-
-
-
-
 
     if (isLoading) {
         return <Loader />;
     }
 
-
-
     return (
         <div className="raceDetails">
             <div>
                 <p></p>
+            </div>
+            <thead>
+                <tr>Qualifying Results</tr>
+                <tr>
+                    <td>Pos</td>
+                    <td>Driver</td>
+                    <td>Team</td>
+                    <td>Best Time</td>
+                </tr>
+            </thead>
+            <tbody>
+                {qualifying.map((driver) => {
+                    return (
+                        <>
+                            <tr>
+                                <td>{driver.position}</td>
+                                <td>{driver.Driver.nationality}{driver.Driver.familyName}</td>
+                                <td>{driver.Constructor.name}</td>
+                                {/* <td>{driver.}</td> */}
+                            </tr>
+                        </>
+
+                    )
+                })}
+            </tbody>
+            <div>
+
             </div>
             <p>Results</p>
         </div>
