7fc507c0439090ac47588a335c2e4e2ba32806ca
Biljana-R <bilja.radeljevic@gmail.com>
2025-05-20 15:55:06 +0200

    teamDetails

--- src/components/TeamDetails.jsx
+++ src/components/TeamDetails.jsx
@@ -7,48 +7,66 @@ export default function TeamDetails() {
     const { teamId } = useParams();
     const [teamDetails, setTeamDetails] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [teamResults, setDriverRaces] = useState([]);
 
     useEffect(() => {
         getTeamDetails();
     }, []);
 
     const getTeamDetails = async () => {
-        console.log(teamId);
-        const url = "http://ergast.com/api/f1/2013/constructors/&{team.id}/constructorStandings.json";
-        const response = await axios.get(url);
-        console.log(response);
-        setTeamDetails(response.data);
+        const teamsStandingsUrl = `http://ergast.com/api/f1/2013/constructors/${team.Id}/constructorStandings.json`;
+        const teamStandingResponse = await axios.get(teamsStandingsUrl);
+        setTeamDetails(teamStandingResponse.data.MRData.Standings[0].TeamStandings[0]);
+
+        const teamResault = `http://ergast.com/api/f1/2013/constructors/${team.Id}/results.json `;
+        const teamResaultResponse = await axios.get(teamResault);
+        setTeamResaults(teamResaultResponse.data.MRData.ResultsTable.Results);
         setIsLoading(false);
-    }
+        console.log("teamResaultResponse", teamResultResponse);
+    };
 
     if (isLoading) {
         return <Loader />;
     }
+
+    console.log("teamResults", teamResults);
+
     return (
-        <div>
-            <div className="teams">
-                <table>
-                    <thead>
-                        <tr>
-                            <th colSpan={4}>
-                                <p>Constructor Championship Standings - 2013</p>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {teams.map((team, i) => {
-                            return (
-                                <tr key={i}>
-                                    <td>{team.position}</td>
-                                    <td>{team.Constructor.name}</td>
-                                    <td>Details</td>
-                                    <td>{team.points}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
+        <div className="team-details">
+            <div>
+                <h2> {teamDetails.Team.teamName} </h2>
+                <p>Country:{teamDetails.Team.country} </p>
+                <p>Position:{teamDetails.position}</p>
+                <p>Points:{teamDetails.points}</p>
+                <p>History:{teamDetails.history}</p>
+                <p><Link to={teamDetails.Team.url} target="_blank">Wikipedia</Link></p>
             </div>
+            <div>
+                <div className="teams">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th colSpan={4}>
+                                    <p>Constructor Championship Standings - 2013</p>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {teams.map((team, i) => {
+                                return (
+                                    <tr key={i}>
+                                        <td>{team.position}</td>
+                                        <td>{team.Constructor.name}</td>
+                                        <td>Details</td>
+                                        <td>{team.points}</td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            );
+        </div>
+
+    );
 }
