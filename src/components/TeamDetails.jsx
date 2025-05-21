c1b716aabf45e586928f0d2626b94e4e1ee542bc
Biljana-R <bilja.radeljevic@gmail.com>
2025-05-21 12:51:49 +0200

    TeamDetailsTable

--- src/components/TeamDetails.jsx
+++ src/components/TeamDetails.jsx
@@ -61,7 +61,7 @@ export default function TeamDetails() {
                 </ul>
             </div>
 
-            <div className="team">
+            <div className="drivers">
                 <table>
                     <thead>
                         <tr>
