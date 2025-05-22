67f94740e3359839c9326373e4f255e74cd3ee22
Srdjan-P <srdjan.potrebic@gmail.com>
2025-05-22 13:05:46 +0200

    add: Select for dynamic year & sass changes

--- src/components/Drivers.jsx
+++ src/components/Drivers.jsx
@@ -1,73 +1,92 @@
 import axios from "axios";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import Loader from "./Loader";
 import { getCodeByNationality } from "../FlagCodes";
 import Flag from "react-flagkit";
 
-export default function Drivers({ countryList }) {
-    const [drivers, setDrivers] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const navigate = useNavigate();
+export default function Drivers({ countryList, selectedYear }) {
+  const [drivers, setDrivers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        getDrivers();
-    }, [])
+  useEffect(() => {
+    getDrivers();
+  }, [selectedYear]);
 
-    const getDrivers = async () => {
-        const url = "http://ergast.com/api/f1/2013/driverStandings.json";
-        const response = await axios.get(url);
-        const data = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-        console.log(data);
-        setDrivers(data);
-        setIsLoading(false);
-    };
+  console.log("drivers", drivers);
 
-    const handleClickDetails = (id) => {
-        console.log(id);
-        const linkTo = `/drivers/${id}`;
-        navigate(linkTo);
-    }
+  const getDrivers = async () => {
+    const url = `http://ergast.com/api/f1/${selectedYear}/driverStandings.json`;
+    const response = await axios.get(url);
+    const data =
+      response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    setDrivers(data);
+    setIsLoading(false);
+  };
 
-    return (
-        <div className="drivers">
-            {isLoading ? <Loader /> :
-                <table>
-                    <thead>
-                        <tr>
-                            <th colSpan={4}>
-                                <p>Drivers Championship Standings - 2013</p>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {drivers.map((driver) => {
-                            return (
-                                <tr key={driver.Driver.driverId}>
-                                    <td style={{ textAlign: "center" }}>
-                                        {driver.position}
-                                    </td>
-                                    <td width="45%"
-                                        onClick={() => { handleClickDetails(driver.Driver.driverId) }}>
-                                        <Flag country={getCodeByNationality(countryList, driver.Driver.nationality)} />
-                                        <span style={{ cursor: "pointer" }}>
-                                            {driver.Driver.givenName} {driver.Driver.familyName}
-                                        </span>
-                                    </td>
-                                    <td width="45%">
-                                        <span style={{ cursor: "pointer" }}>
-                                            {driver.Constructors[0].name}
-                                        </span>
-                                    </td>
-                                    <td style={{ textAlign: "center" }}>
-                                        {driver.points}
-                                    </td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-            }
-        </div>
-    )
+  const handleClickDetails = (id) => {
+    const linkTo = `/drivers/${id}`;
+    navigate(linkTo);
+  };
+
+  const handleClickTeams = (id) => {
+    const linkTo = `/teams/${id}`;
+    navigate(linkTo);
+  };
+
+  return (
+    <div className="drivers">
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th colSpan={4}>
+                <p>Drivers Championship Standings - {selectedYear}</p>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {drivers.map((driver) => {
+              return (
+                <tr key={driver.Driver.driverId}>
+                  <td style={{ textAlign: "center" }}>{driver.position}</td>
+                  <td
+                    width="45%"
+                    onClick={() => {
+                      handleClickDetails(driver.Driver.driverId);
+                    }}
+                  >
+                    <span style={{ cursor: "pointer" }}>
+                      <Flag
+                        className="flag"
+                        country={getCodeByNationality(
+                          countryList,
+                          driver.Driver.nationality
+                        )}
+                      />
+                      {driver.Driver.givenName} {driver.Driver.familyName}
+                    </span>
+                  </td>
+                  <td width="45%">
+                    <span
+                      style={{ cursor: "pointer" }}
+                      onClick={() =>
+                        handleClickTeams(driver.Constructors[0].constructorId)
+                      }
+                    >
+                      {driver.Constructors[0].name}
+                    </span>
+                  </td>
+                  <td style={{ textAlign: "center" }}>{driver.points}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
---
+++ src/components/Select.jsx
@@ -0,0 +1,71 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+export default function BasicSelect({array, value, onChange}) {
+  return (
+    <Box sx={{ minWidth: 90 }}>
+      <FormControl fullWidth>
+        <InputLabel
+          id="demo-simple-select-label"
+          sx={{
+            color: "Text.secondary",
+            fontWeight: "600",
+          }}
+        >
+          {"Years"}
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={value}
+          label="Year"
+          onChange={onChange}
+          sx={{
+            backgroundColor: "#f0f0f0",
+            "& .MuiOutlinedInput-notchedOutline": {
+              borderColor: "red",
+            },
+            "&:hover .MuiOutlinedInput-notchedOutline": {
+              borderColor: "blue",
+            },
+          }}
+          MenuProps={{
+            PaperProps: {
+              sx: {
+                backgroundColor: "#f5f5f5",
+                marginTop: "8px",
+                maxHeight: 200,
+                "& .MuiMenuItem-root": {
+                  "&:hover": {
+                    backgroundColor: "red",
+                    color: "#fff",
+                    fontWeight: "600",
+                  },
+                  "&.Mui-selected": {
+                    backgroundColor: "#d0d0d0",
+                    "&:hover": {
+                      backgroundColor: "red",
+                    },
+                  },
+                },
+              },
+            },
+        }}
+        >
+
+          {array.map((item) => {
+            return (
+              <MenuItem key={item} value={item}>
+                {item}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
