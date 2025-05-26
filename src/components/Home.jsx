3abceb1f644f354174e8c50fc404cb2d9748ef33
MarinaInCodeLand <jakov.marina@gmail.com>
2025-05-26 12:06:54 +0200

    Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,20 @@
 export default function Home() {
     return (
-        <div className="photo-container">
-        <div className="photo-one"></div>
-        <div className="photo-two"></div>
-        <div className="photo-three"></div>
-        <div className="photo-four"></div>
+        <div>
+            <div className="home-content">
+
+            </div>
+
+            <footer className="footer">
+                <div className="footer-left">
+                    <img src="/img/Logo.png" alt="Logo" className="logo-footer" />
+                </div>
+                <div className="footer-right">
+                    <p>&copy; 2025 All right reserved.</p>
+
+                </div>
+
+            </footer>
         </div>
 
     );
