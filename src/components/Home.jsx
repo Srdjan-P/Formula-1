6edb13865f8cb1f48312dd69dac236da33382f75
Olivera077 <bellisperennis077@gmailcom>
2025-05-27 13:36:10 +0200

    Home, HomeScss, raceDetails izmene

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,10 @@ export default function Home() {
 
             <header>
                 <div className="header-container">
-                    <img className="flag" src="../img/ChecheredFlag.avif" alt="" srcset="" />
+                    {/* <img className="flag" src="../img/ChecheredFlag.avif" alt="" srcset="" /> */}
+
                     <div className="welcome-container">
-                        <h2 className="welcome">Experience the adrenaline of the F1 universe!</h2>
+                        <h2 className="welcome">Experience the adrenaline of the F1 universe</h2>
 
                     </div>
 
@@ -16,32 +17,29 @@ export default function Home() {
             </header>
 
             <main>
-                <div className="home-content">
+                <div className="homepage-container dark-theme">
 
-                    <div class="carousel-container">
-                        <img src="../img/carousel1.jpg" alt="formula f" />
-                        <img src="../img/carousel2.avif" alt="formula f" />
-                        <img src="../img/carousel3.avif" alt="formula f" />
-                        <img src="../img/carousel4.jpg" alt="formula f" />
-                        <img src="../img/carousel5.jpg" alt="formula f" />
-                        <img src="../img/carousel6.png" alt="formula f" />
 
-                        <img src="../img/carousel1.jpg" alt="formula f" />
-                        <img src="../img/carousel2.avif" alt="formula f" />
-                        <img src="../img/carousel3.avif" alt="formula f" />
-                        <img src="../img/carousel4.jpg" alt="formula f" />
-                        <img src="../img/carousel5.jpg" alt="formula f" />
-                        <img src="../img/carousel6.png" alt="formula f" />
 
+                    <div className="home-content">
+
+                        <div class="carousel-container">
+                            <img src="../img/carousel1.jpg" alt="formula f" />
+                            <img src="../img/carousel2.avif" alt="formula f" />
+                            <img src="../img/carousel3.avif" alt="formula f" />
+                            <img src="../img/carousel4.jpg" alt="formula f" />
+                            <img src="../img/carousel5.jpg" alt="formula f" />
+                            <img src="../img/carousel6.png" alt="formula f" />
+
+                        </div>
 
                     </div>
 
-                </div>
+                    <div className="video-monaco">
 
-                <div className="video-monaco">
+                        <video src="../img/video/formulaVideo2.mp4" controls width="700" autoplay muted></video>
 
-                    <video src="../img/video/formulaVideo2.mp4" controls width="800" autoplay muted></video>
-
+                    </div>
                 </div>
             </main>
             <footer>
