3d6b05345f68dce1453cbd72d8ca5605c7bcc193
Srdjan-P <srdjan.potrebic@gmail.com>
2025-05-29 13:59:11 +0200

    Bug fixes

--- src/components/BasicSelect.jsx
+++ src/components/BasicSelect.jsx
@@ -18,7 +18,7 @@ export default function BasicSelect({ array, value, onChange }) {
             }
           }}
         >
-          {"Years"}
+          Years
         </InputLabel>
         <Select
           labelId="select-label"
