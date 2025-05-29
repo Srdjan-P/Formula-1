e6c012601a026fd8127557bb6eedc5e19ba8eb63
Olivera077 <bellisperennis077@gmailcom>
2025-05-29 15:58:31 +0200

    Home fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,3 @@
-import { Link } from "@mui/material";
-import Footer from "./Footer";
-
 export default function Home() {
     return (
         <div className="homepage-container">
@@ -9,73 +6,41 @@ export default function Home() {
                 <h2 className="welcome">Experience the adrenaline of the F1 universe</h2>
             </header>
             <div className="checkered-board-1">
-                {/* <h2 className="checkered-text">Legendary Race Tracks Await</h2> */}
             </div>
-
             <main>
                 <div className="homepage-container dark-theme">
-
-
-
-                    {/* <div className="carousel-wrapper">
-
-                        <div className="carousel-container">
-
-                            <img src="../img/carousel1.jpg" alt="formula f" />
-                            <img src="../img/carousel2.avif" alt="formula f" />
-                            <img src="../img/carousel3.avif" alt="formula f" />
-
-                            <img src="../img/carousel1.jpg" alt="formula f" />
-                            <img src="../img/carousel2.avif" alt="formula f" />
-                            <img src="../img/carousel3.avif" alt="formula f" />
-
-                        </div>
-                       
-
-                    </div> */}
-
-                    <div className="kartice-div">
-
+                    <div className="cards-div">
                         <div><p className="header-h3">The Most Stunning Race Tracks in the World</p></div>
-
-                        <div className="kartice">
-
-                            <div className="kartice">
-                                <div className="kartica">
-                                    <a href="https://www.formula1.com/en/racing/2024/Monaco.html" target="_blank">
-                                        <img src="../img/circuitDeMonaco.jpg" alt="Circuit de Monaco" />
-                                    </a>
-                                </div>
-
-                                <div className="kartica">
-                                    <a href="https://www.formula1.com/en/racing/2024/Great_Britain.html" target="_blank">
-                                        <img src="../img/circuitDeSilverstone.jpg" alt="Silverstone Circuit" />
-                                    </a>
-                                </div>
-
-                                <div className="kartica">
-                                    <a href="https://www.formula1.com/en/racing/2024/Hungary.html" target="_blank">
-                                        <img src="../img/circuitDeHungaroring.jpg" alt="Hungaroring Circuit" />
-                                    </a>
-                                </div>
-
-                                <div className="kartica">
-                                    <a href="https://www.formula1.com/en/racing/2024/Japan.html" target="_blank">
-                                        <img src="../img/circuitDeSuzuka.jpg" alt="Suzuka International Racing Course" />
-                                    </a>
-                                </div>
+                        <div className="cards">
+                            <div className="card">
+                                <a href="https://www.formula1.com/en/racing/2024/Monaco.html" target="_blank">
+                                    <img src="../img/circuitDeMonaco.jpg" alt="Circuit de Monaco" />
+                                </a>
                             </div>
 
-                        </div>
+                            <div className="card">
+                                <a href="https://www.formula1.com/en/racing/2024/Great_Britain.html" target="_blank">
+                                    <img src="../img/circuitDeSilverstone.jpg" alt="Silverstone Circuit" />
+                                </a>
+                            </div>
 
-                        {/* <video src="../img/video/formulaVideo2.mp4" controls width="700" autoplay muted></video> */}
+                            <div className="card">
+                                <a href="https://www.formula1.com/en/racing/2024/Hungary.html" target="_blank">
+                                    <img src="../img/circuitDeHungaroring.jpg" alt="Hungaroring Circuit" />
+                                </a>
+                            </div>
+
+                            <div className="card">
+                                <a href="https://www.formula1.com/en/racing/2024/Japan.html" target="_blank">
+                                    <img src="../img/circuitDeSuzuka.jpg" alt="Suzuka International Racing Course" />
+                                </a>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
 
-                <div className="checkered-board">
-                    {/* <h2 className="checkered-text">Legendary Race Tracks Await</h2> */}
-                </div>
+                <div className="checkered-board"></div>
 
 
                 <section className="sponsors">
